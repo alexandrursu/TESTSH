a519fdecb6ebfccef23517299f166db040e630b2
unknown <alexandru.ursu@Laptop277.intern.arobs.com>
2015-04-01 16:37:40 +0300

    final project

---
+++ app/failedsearch/controllers/failedsearchCtrl.js
@@ -0,0 +1,12 @@
+/**
+ * Created by alexandru.ursu on 3/25/2015.
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('failedSearch',[]).controller('CollapseDemoCtrl', function ($scope) {
+        $scope.isCollapsed = false;
+    });
+
+})();
