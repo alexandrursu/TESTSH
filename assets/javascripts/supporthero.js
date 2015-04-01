4fd1c68fd601e727457f30f7e5201baa7524e0e4
unknown <alexandru.ursu@Laptop277.intern.arobs.com>
2015-04-01 16:52:50 +0300

    removed some extra js code

--- assets/javascripts/supporthero.js
+++ assets/javascripts/supporthero.js
@@ -10,16 +10,3 @@ $(function() {
 if (!window.supporthero) {
     window.supporthero = {};
 }
-
-    // .position() uses position relative to the offset parent,
-    var pos = $(".ticket--list .btn--add").position();
-
-    // .outerWidth() takes into account border and padding.
-    var width = $(".ticket--list .btn--add").outerWidth();
-
-    //show the menu directly over the placeholder
-    $(".ticket--list .list-group-item__action").css({
-        position: "absolute",
-        top: pos.top + "px",
-        left: (pos.left + width) + "px"
-    }).show();
